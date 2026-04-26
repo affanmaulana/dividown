0af0976e4f278b0d77c31663620ca1c055f1e33e
Affan Maulana Yasin <affanmaulana58@gmail.com>
2026-04-26 16:28:07 +0700

    Setup project and initial data

---
+++ dividown/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-emerald-400">Dividown is Ready.</h1>
+    </div>
+  )
+}
+export default App
