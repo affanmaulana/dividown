a6cbbee15101dbc3372b9233822e8a5f5cb7d191
Affan Maulana Yasin <affanmaulana58@gmail.com>
2026-05-02 18:39:33 +0700

    scroll point

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,3 +1,4 @@
+import { useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
@@ -5,6 +6,10 @@ import { Analytics } from "@vercel/analytics/react";
 
 export default function Layout() {
   const location = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   return (
     <div className="min-h-screen bg-slate-50 selection:bg-indigo-100 selection:text-indigo-900">
